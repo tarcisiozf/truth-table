eae1d0e48afc4c4ab2b8987a8a5b98c53c76c2ac
Tarcisio ZF <tar-2008.ferraz@hotmail.com>
2017-12-30 19:52:12 -0200

    it works :)

---
+++ js/main.js
@@ -0,0 +1,153 @@
+class Main {
+	constructor() {
+		this.solveExp = this.solveExp.bind(this)
+		this.printSymbol = this.printSymbol.bind(this)
+
+		this.true = 'T'
+		this.false = 'F'
+		this.order = true
+
+		this.message = document.getElementById('message')
+		this.result = document.getElementById('result')
+		this.input = document.getElementById('in_exp')
+
+		this.input.addEventListener('keyup', this.solveExp)
+		this.input.focus()
+
+		this.renderButtons()
+	}
+
+	solveExp() {
+		const exp_str = this.input.value.trim()
+
+		if ( exp_str.length === 0 ) {
+			this.clearMessage()
+			return
+		}
+
+		this.loadOptions()
+
+		try {
+			checkMatchingParentheses(exp_str)
+
+			const [exp, vars] = parse(exp_str)
+			const table = createTable(vars.length, this.order)
+
+			const results = table.map(values =>
+				solve(exp, bindValues(vars, values)))
+
+			this.clearMessage()
+			this.renderResult(table, vars, results)
+		} catch (e) {
+			this.setErrorMessage(e)
+		}
+	}
+
+	setMessage(msg, color) {
+		this.message.textContent = msg
+		this.message.style.color = color
+	}
+
+	clearMessage() {
+		this.setMessage('Type your expression', '#066b9e')
+	}
+
+	setErrorMessage(msg) {
+		this.setMessage(msg, '#9e0606')
+		this.clearResults()
+	}
+
+	renderButtons() {
+		const wrapper = document.getElementById('symbols')
+
+		for(const symbol of Object.keys(symbols)) {
+
+			const name = symbols[symbol].name.toUpperCase()
+
+			const button = document.createElement('button')
+			button.textContent = symbol
+			button.alt = name
+			button.title = name
+			button.addEventListener('click', this.printSymbol)
+
+			wrapper.appendChild(button)
+		}
+	}
+
+	printSymbol(event) {
+		const start = this.input.selectionStart
+		const end = this.input.selectionEnd
+		const value = this.input.value
+		const symbol = event.target.textContent
+
+		this.input.value =
+			value.substr(0, start)
+			+ symbol
+			+ value.substr(end)
+
+		this.input.focus()
+		this.input.setSelectionRange(end + 1, end + 1)
+		this.solveExp()
+	}
+
+	renderResult(bool_table, vars, results) {
+		const table = document.createElement('table')
+
+		table.appendChild(this.renderRow(vars, '\tResult\t'))
+
+		for(let i = 0; i < bool_table.length; i++) {
+			table.appendChild(this.renderRow(bool_table[i], results[i]))
+		}
+
+		this.clearResults()
+		this.result.appendChild(table)
+	}
+
+	renderRow(vars, result) {
+		const row = vars.slice(0)
+		row.push(result)
+
+		const tr = document.createElement('tr')
+
+		for(let value of row) {
+			const td = document.createElement('td')
+
+			if (typeof value === 'boolean') {
+				value = value
+					? this.true
+					: this.false
+			} else {
+				value = '<b>' + value.toUpperCase() + '</b>'
+			}
+
+			td.innerHTML = value
+
+			tr.appendChild(td)
+		}
+
+		return tr
+	}
+
+	clearResults() {
+		while(this.result.firstChild) {
+			this.result.removeChild(this.result.firstChild)
+		}
+	}
+
+	loadOptions() {
+		const constants = document.querySelector('input[name=constants]:checked').value
+		const order = document.querySelector('input[name=order]:checked').value
+
+		this.order = order === 'T -> F'
+
+		if ( constants === 'T/F' ) {
+			this.true = 'T'
+			this.false = 'F'
+		} else {
+			this.true = '1'
+			this.false = '0'
+		}
+	}
+}
+
+window.onload = () => new Main()
---
+++ js/truth-table.js
@@ -0,0 +1,194 @@
+// ¬p
+const not = p => !p
+
+// p ∧ q
+const and = (p, q) => p && q
+
+// p ∨ q
+const or = (p, q) => p || q
+
+// ¬p ∨ q
+const implication = (p, q) => or(not(p), q)
+
+// (p ∧ q) ∨ (¬p ∧ ¬q)
+const equality = (p, q) => or(and(p, q), and(not(p), not(q)))
+
+// (p ∧ ¬q) ∨ (¬p ∧ q)
+const xor = (p, q) => or(and(p, not(q)), and(not(p), q))
+
+//  p ↑ q
+const nand = (p, q) => not(and(p, q))
+
+// p ↓ q
+const nor = (p, q) => not(or(p, q))
+
+const symbols = {
+    '¬': not,
+    '~': not,
+    '∧': and,
+    '&': and,
+    '.': and,
+    '∨': or,
+    '+': or,
+    '⇒': implication,
+    '→': implication,
+    '↔': equality,
+    '=': equality,
+    '≡': equality,
+    '⊕': xor,
+    '≠': xor,
+    '↑': nand,
+    '|': nand,
+    '↓': nor
+}
+
+const isVar = c => ( ( c >= 'A' && c <= 'Z' ) || ( c >= 'a' && c <= 'z' ) )
+
+const isFunc = c => c in symbols
+
+const createMatrix = (n, m) => {
+    const out = new Array(n)
+
+    for(let i = 0; i < n; i++) {
+        out[i] = new Array(m)
+    }
+
+    return out
+}
+
+const createTable = (n, order) => {
+    const len = Math.pow(2, n)
+    const matrix = createMatrix(len, n)
+
+    let pivot = len
+
+    for(let i = 0; i < n; i++) {
+        let acc = 0
+        let val = order
+
+        pivot = pivot / 2
+
+        for(let j = 0; j < len; j++) {
+
+            if ( acc === pivot ) {
+                val = ! val
+                acc = 0
+            }
+
+            matrix[j][i] = val
+            acc++
+        }
+    }
+
+    return matrix
+}
+
+const parse = (str, vars = [], offset = 0) => {
+    let out = []
+    let val, i
+
+    for(i = offset; i < str.length; i++) {
+        const c = str[i]
+
+        if ( c === '(' ) {
+            [val, vars, i] = parse(str, vars, i + 1)
+        }
+        else if ( c === ')' ) {
+            break
+        }
+        else if ( isVar(c) ) {
+            val = c
+
+            if ( ! vars.includes(c) ) {
+                vars.push(c)
+            }
+        }
+        else if ( isFunc(c) ) {
+            val = c
+        }
+        else {
+            continue
+        }
+
+        out.push(val)
+    }
+
+    return [out, vars, i]
+}
+
+const checkMatchingParentheses = str => {
+    let open = 0
+    let close = 0
+
+    for(let i = 0; i < str.length; i++) {
+        const c = str[i]
+
+        if ( c === '(' ) {
+            open++
+        }
+        else if ( c === ')' ) {
+            close++
+        }
+    }
+
+    if ( open !== close ) {
+	    throw new SyntaxError('Number of parentheses does not match')
+    }
+}
+
+const bindValues = (names, values) => {
+    const out = {}
+
+    for(let i = 0; i < names.length; i++) {
+        out[names[i]] = values[i]
+    }
+
+    return out
+}
+
+const groupExpression = (exp) => {
+    const first = exp.slice(0, 3)
+    const tail = exp.slice(3)
+    const out = [ first ].concat(tail)
+
+    return ( out.length > 1 )
+        ? groupExpression(out)
+        : out
+}
+
+const solve = (exp, values) => {
+
+    const len = exp.length
+
+    if ( len === 1 ) {
+        if ( exp instanceof Array ) {
+	        return solve(exp[0], values)
+        }
+        else if ( exp in values ) {
+        	return values[exp]
+        }
+        else {
+        	throw new SyntaxError(`Unexpected '${exp}' in expression`)
+        }
+    }
+    else if ( len === 2 ) {
+        const [ symbol, p ] = exp
+        const func = symbols[symbol]
+
+        if ( func !== not ) {
+            throw new SyntaxError(`Could not solve pair <${symbol}> <${p}>`)
+        }
+
+        return not(p)
+    }
+    else if ( len === 3 ) {
+        const [ p, symbol, q ] = exp
+        const func = symbols[symbol]
+
+        return func(solve(p, values), solve(q, values))
+    }
+    else {
+        return solve(groupExpression(exp), values)
+    }
+
+}
